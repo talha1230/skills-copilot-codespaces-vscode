0ba0e6dfe19f6a33c8f215bb065f8700b45377d3
Talha Muhammad <121675123+talha1230@users.noreply.github.com>
2024-10-12 13:09:26 +0000

    Copilot Second Commit

---
+++ member.js
@@ -0,0 +1,9 @@
+function skillsMember()
+{
+    // Add a new skill to the member
+    var member = new Member();
+    member.addSkill("JavaScript");
+    member.addSkill("CSS");
+    member.addSkill("HTML");
+}
+
